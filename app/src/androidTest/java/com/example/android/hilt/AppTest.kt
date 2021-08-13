10061c61dab3748bcb3945788bd6a62c48773ba2
Sergio <webanimal.group@gmail.com>
2021-08-13 14:55:42 +0300

    Add custom test instrumentation runner with Hilt

--- app/src/androidTest/java/com/example/android/hilt/AppTest.kt
+++ app/src/androidTest/java/com/example/android/hilt/AppTest.kt
@@ -25,17 +25,19 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.hilt.ui.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.hamcrest.Matchers.containsString
-import org.junit.After
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class AppTest {
-
-    @After
-    fun tearDown() {
-    }
+    
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Test
     fun happyPath() {
